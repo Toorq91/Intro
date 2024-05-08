2c5221a1fd8d9add28631ad64aa37f86d8946be3
Toorq91 <157486467+Toorq91@users.noreply.github.com>
2024-05-08 10:40:17 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -20,14 +20,26 @@ namespace My_Awesome_Program
             string animalName = Console.ReadLine();
             Console.WriteLine($"{animalName} asked him what he wanted to drink?");
             string drinkName = Console.ReadLine();
-            Console.WriteLine($"...{drinkName} shouted {knightName}! \nThe {animalName} shushed him quickly.");
+            Console.WriteLine($"...{drinkName} shouted {knightName}!");
+
+            // Random 50% chance outcome
 
             bool enounterDragon = new Random().Next(0, 2) == 0;
 
-            if (enounterDragon)
+            // Will get a different outcome if writing vine, Vine, VINE or shot, Shot, SHOT as the choice of drink.
+
+            if (drinkName.ToLower() == "vine" || drinkName.ToLower() == "shot")
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine($"{knightName} ordered {drinkName}, an interesting choice indeed!");
+                Console.WriteLine($"Suddenly, a wizard appeared from a nearby table, intrigued by {knightName}'s choice of drink.");
+                Console.WriteLine($"The wizard offered {knightName} a magical potion, and {knightName} accepted, gulping the potion down {knightName} got the feeling of being invincible!");
+            }
+                else if (enounterDragon)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"But it was too late...\nA dragon swooped in and hit {knightName}. \nSo hard in fact that all the {drinkName} spilt on the floor and it also left a cut on {knightName}'s arm.");
+                Console.WriteLine($"The {animalName} shushed him quickly.");
+                Console.WriteLine($"But it was too late...\nA dragon swooped in and hit {knightName}. \nSo hard in fact that all the {drinkName} spilt on the floor and left a cut on {knightName}'s arm.");
                 Console.WriteLine($"{knightName} fought valiantly but alas, it was not enough. The dragon devoured {knightName} and the {animalName}. The end.");
             }
             else
@@ -36,7 +48,8 @@ namespace My_Awesome_Program
                 Console.WriteLine($"{knightName} enjoyed the {drinkName} and had a pleasant conversation with {animalName}. ");
                 Console.WriteLine($"Feeling satisfied, {knightName} left the bar and embarked on a new adventure. The end!");
             }
-           
+
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.ReadLine();
             Console.WriteLine("Yes, Finished, FINITO... THE END! :)");
             Console.ReadKey();
